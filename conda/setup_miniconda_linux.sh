7712ae968fe33bf08326e45351be8da82ee3e6b1
Ho Trong Hien <115549171+hienhayho@users.noreply.github.com>
2025-02-21 22:54:03 +0700

    Update setup_miniconda_linux.sh

--- conda/setup_miniconda_linux.sh
+++ conda/setup_miniconda_linux.sh
@@ -7,7 +7,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 
+conda init --all
+
 # shellcheck source=/dev/null
 source ~/miniconda3/bin/activate
-
-conda init --all
